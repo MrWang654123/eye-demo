af47b1256655254ef0da893b18f951edc93bd679
shiro <shiro@cheersmind.com>
2018-10-24 19:43:37 +0800

    测评列表页面中线性布局模式响应量表开启成功和问题提交成功：直接刷新

--- app/src/main/java/com/cheersmind/cheersgenie/features/modules/exam/fragment/ExamDoingFragment.java
+++ app/src/main/java/com/cheersmind/cheersgenie/features/modules/exam/fragment/ExamDoingFragment.java
@@ -2,6 +2,7 @@ package com.cheersmind.cheersgenie.features.modules.exam.fragment;
 
 import android.view.View;
 
+import com.cheersmind.cheersgenie.features.adapter.ExamDimensionRecyclerAdapter;
 import com.cheersmind.cheersgenie.features.constant.Dictionary;
 import com.cheersmind.cheersgenie.features.entity.RecyclerCommonSection;
 import com.cheersmind.cheersgenie.features.event.DimensionOpenSuccessEvent;
@@ -46,45 +47,55 @@ public class ExamDoingFragment extends ExamBaseFragment {
                 refreshChildTopList();
 
             } else {
-                //局部刷新量表对应的视图项
-                List<RecyclerCommonSection<DimensionInfoEntity>> data = recyclerAdapter.getData();
-                if (ArrayListUtil.isNotEmpty(data)) {
-                    //header模型位置
-                    int headerPosition = 0;
+                //网格布局
+                if (recyclerAdapter instanceof ExamDimensionRecyclerAdapter) {
+                    //局部刷新量表对应的视图项
+                    List<RecyclerCommonSection<DimensionInfoEntity>> data = recyclerAdapter.getData();
+                    if (ArrayListUtil.isNotEmpty(data)) {
+                        //header模型位置
+                        int headerPosition = 0;
 
-                    for (int i = 0; i < data.size(); i++) {
-                        RecyclerCommonSection<DimensionInfoEntity> item = data.get(i);
-                        TopicInfoEntity topicInfo = (TopicInfoEntity) item.getInfo();
-                        DimensionInfoEntity t = (DimensionInfoEntity) item.t;
+                        for (int i = 0; i < data.size(); i++) {
+                            RecyclerCommonSection<DimensionInfoEntity> item = data.get(i);
+                            TopicInfoEntity topicInfo = (TopicInfoEntity) item.getInfo();
+                            DimensionInfoEntity t = (DimensionInfoEntity) item.t;
 
-                        //t不为空
-                        if (t != null ) {
-                            //找出同一个量表，设置孩子量表，然后局部刷新列表项
-                            if (t.getTopicId().equals(dimension.getTopicId())
-                                    && t.getDimensionId().equals(dimension.getDimensionId())) {
-                                //孩子话题如果为空，则创建孩子话题对象，并设置为未完成状态
-                                TopicInfoChildEntity childTopic = topicInfo.getChildTopic();
-                                if (childTopic == null) {
-                                    childTopic = new TopicInfoChildEntity();
-                                    childTopic.setStatus(Dictionary.TOPIC_STATUS_INCOMPLETE);
-                                    topicInfo.setChildTopic(childTopic);
+                            //t不为空
+                            if (t != null) {
+                                //找出同一个量表，设置孩子量表，然后局部刷新列表项
+                                if (t.getTopicId().equals(dimension.getTopicId())
+                                        && t.getDimensionId().equals(dimension.getDimensionId())) {
+                                    //孩子话题如果为空，则创建孩子话题对象，并设置为未完成状态
+                                    TopicInfoChildEntity childTopic = topicInfo.getChildTopic();
+                                    if (childTopic == null) {
+                                        childTopic = new TopicInfoChildEntity();
+                                        childTopic.setStatus(Dictionary.TOPIC_STATUS_INCOMPLETE);
+                                        topicInfo.setChildTopic(childTopic);
+                                    }
+
+                                    //刷新对应量表的列表项
+                                    t.setChildDimension(dimension.getChildDimension());//重置孩子量表对象
+                                    int tempPosition = i + recyclerAdapter.getHeaderLayoutCount();
+                                    recyclerAdapter.notifyItemChanged(tempPosition);//局部数显列表项，把header计算在内
+
+                                    break;
                                 }
 
-                                //刷新对应量表的列表项
-                                t.setChildDimension(dimension.getChildDimension());//重置孩子量表对象
-                                int tempPosition = i + recyclerAdapter.getHeaderLayoutCount();
-                                recyclerAdapter.notifyItemChanged(tempPosition);//局部数显列表项，把header计算在内
-
-                                break;
+                            } else {
+                                //t为空则代表是header模型
+                                headerPosition = i;
                             }
 
-                        } else {
-                            //t为空则代表是header模型
-                            headerPosition = i;
                         }
-
                     }
+
+                } else {
+                    //重置为第一页
+                    resetPageInfo();
+                    //刷新孩子话题
+                    refreshChildTopList();
                 }
+
             }
         }
     }
