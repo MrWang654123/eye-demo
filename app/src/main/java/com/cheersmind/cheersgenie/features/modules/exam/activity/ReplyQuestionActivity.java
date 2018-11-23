5a43ed1c775cc4c4e705239d7c1db48758295b43
shiro <shiro@cheersmind.com>
2018-11-23 19:57:37 +0800

    答题页百度语音初始化调优

--- app/src/main/java/com/cheersmind/cheersgenie/features/modules/exam/activity/ReplyQuestionActivity.java
+++ app/src/main/java/com/cheersmind/cheersgenie/features/modules/exam/activity/ReplyQuestionActivity.java
@@ -350,43 +350,47 @@ public class ReplyQuestionActivity extends BaseActivity implements VoiceButtonUI
      * FileSaveListener 在UiMessageListener的基础上，使用 onSynthesizeDataArrived回调，获取音频流
      */
     protected void initialTts() {
-        try {
-            LoggerProxy.printable(true); // 日志打印在logcat中
-            // 设置初始化参数
-            // 此处可以改为 含有您业务逻辑的SpeechSynthesizerListener的实现类
-            SpeechSynthesizerListener listener = new UiMessageListener(mHandler);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+//            LoggerProxy.printable(true); // 日志打印在logcat中
+                    // 设置初始化参数
+                    // 此处可以改为 含有您业务逻辑的SpeechSynthesizerListener的实现类
+                    SpeechSynthesizerListener listener = new UiMessageListener(mHandler);
 
-            Map<String, String> params = getParams();
+                    Map<String, String> params = getParams();
 
-            // appId appKey secretKey 网站上您申请的应用获取。注意使用离线合成功能的话，需要应用中填写您app的包名。包名在build.gradle中获取。
-            InitConfig initConfig = new InitConfig(Constant.BAIDU_APP_ID, Constant.BAIDU_APP_KEY, Constant.BAIDU_SECRET_KEY, Constant.ttsMode, params, listener);
+                    // appId appKey secretKey 网站上您申请的应用获取。注意使用离线合成功能的话，需要应用中填写您app的包名。包名在build.gradle中获取。
+                    InitConfig initConfig = new InitConfig(Constant.BAIDU_APP_ID, Constant.BAIDU_APP_KEY, Constant.BAIDU_SECRET_KEY, Constant.ttsMode, params, listener);
 
-            // 如果您集成中出错，请将下面一段代码放在和demo中相同的位置，并复制InitConfig 和 AutoCheck到您的项目中
-            // 上线时请删除AutoCheck的调用
-            //非生产环境
-            String hostType = BuildConfig.HOST_TYPE;
-            if(!"product".equals(hostType)) {
-                AutoCheck.getInstance(getApplicationContext()).check(initConfig, new Handler() {
-                    @Override
-                    public void handleMessage(Message msg) {
-                        if (msg.what == 100) {
-                            AutoCheck autoCheck = (AutoCheck) msg.obj;
-                            synchronized (autoCheck) {
-                                String message = autoCheck.obtainDebugMessage();
-                                toPrint(message); // 可以用下面一行替代，在logcat中查看代码
-                                // Log.w("AutoCheckMessage", message);
-                            }
-                        }
-                    }
+                    // 如果您集成中出错，请将下面一段代码放在和demo中相同的位置，并复制InitConfig 和 AutoCheck到您的项目中
+                    // 上线时请删除AutoCheck的调用
+                    //非生产环境
+//            String hostType = BuildConfig.HOST_TYPE;
+//            if(!"product".equals(hostType)) {
+//                AutoCheck.getInstance(getApplicationContext()).check(initConfig, new Handler() {
+//                    @Override
+//                    public void handleMessage(Message msg) {
+//                        if (msg.what == 100) {
+//                            AutoCheck autoCheck = (AutoCheck) msg.obj;
+//                            synchronized (autoCheck) {
+//                                String message = autoCheck.obtainDebugMessage();
+//                                toPrint(message); // 可以用下面一行替代，在logcat中查看代码
+//                                // Log.w("AutoCheckMessage", message);
+//                            }
+//                        }
+//                    }
+//
+//                });
+//            }
+                    synthesizer = new NonBlockSyntherizer(ReplyQuestionActivity.this, initConfig, mHandler); // 此处可以改为MySyntherizer 了解调用过程
 
-                });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-            SpeechSynthesizer instance = SpeechSynthesizer.getInstance();
-            synthesizer = new NonBlockSyntherizer(this, initConfig, mHandler); // 此处可以改为MySyntherizer 了解调用过程
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        }).start();
 
     }
 
