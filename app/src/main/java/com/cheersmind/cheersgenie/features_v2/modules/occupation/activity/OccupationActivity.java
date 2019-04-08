b4cd65756e36ddfab155cc310ca812660b3ea2c9
jerry <jerry@cheersmind2019>
2019-04-08 16:37:34 +0800

    职业列表筛选条件布局优化

--- app/src/main/java/com/cheersmind/cheersgenie/features_v2/modules/occupation/activity/OccupationActivity.java
+++ app/src/main/java/com/cheersmind/cheersgenie/features_v2/modules/occupation/activity/OccupationActivity.java
@@ -5,18 +5,40 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cheersmind.cheersgenie.R;
 import com.cheersmind.cheersgenie.features.modules.base.activity.BaseActivity;
+import com.cheersmind.cheersgenie.features.utils.DataCheckUtil;
+import com.cheersmind.cheersgenie.features.utils.SoftInputUtil;
 import com.cheersmind.cheersgenie.features_v2.interfaces.BackPressedHandler;
-import com.cheersmind.cheersgenie.features_v2.modules.college.fragment.CollegeRankFragment;
 import com.cheersmind.cheersgenie.features_v2.modules.occupation.fragment.OccupationFragment;
-import com.cheersmind.cheersgenie.features_v2.modules.trackRecord.fragment.TrackRecordFragment;
+import com.cheersmind.cheersgenie.main.util.ToastUtil;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 职业
  */
 public class OccupationActivity extends BaseActivity implements BackPressedHandler {
+
+    @BindView(R.id.et_search)
+    EditText mEtSearch;
+    @BindView(R.id.iv_clear)
+    ImageView mIvClear;
+
+    private OccupationFragment fragment;
 
     /**
      * * 启动职业页面
@@ -29,19 +51,63 @@ public class OccupationActivity extends BaseActivity implements BackPressedHandl
 
     @Override
     protected int setContentView() {
-        return R.layout.activity_common_white_title_fragment;
+        return R.layout.activity__occupation;
     }
 
     @Override
     protected String settingTitle() {
-        return "职业大全";
+        return null;
     }
-
 
     @Override
     protected void onInitView() {
         //修改状态栏颜色
         setStatusBarBackgroundColor(OccupationActivity.this, getResources().getColor(R.color.white));
+
+        //清空按钮的显隐
+        mEtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() > 0) {
+                    if (mIvClear.getVisibility() == View.GONE) {
+                        mIvClear.setVisibility(View.VISIBLE);
+                    }
+                } else {
+                    if (mIvClear.getVisibility() == View.VISIBLE) {
+                        mIvClear.setVisibility(View.GONE);
+                        fragment.Search("");
+                    }
+                }
+            }
+        });
+
+        //监听搜索输入框的软键盘回车键
+        mEtSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                //当actionId == XX_SEND 或者 XX_DONE时都触发
+                //或者event.getKeyCode == ENTER 且 event.getAction == ACTION_DOWN时也触发
+                //注意，这是一定要判断event != null。因为在某些输入法上会返回null。
+                if (actionId == EditorInfo.IME_ACTION_SEND
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || (keyEvent != null && KeyEvent.KEYCODE_ENTER == keyEvent.getKeyCode() && KeyEvent.ACTION_DOWN == keyEvent.getAction())) {
+                    //搜索文章
+                    Search(mEtSearch.getText().toString());
+                }
+
+                return false;
+            }
+        });
     }
 
     @Override
@@ -52,7 +118,7 @@ public class OccupationActivity extends BaseActivity implements BackPressedHandl
         //空则添加
         if (fragmentByTag == null) {
             //职业
-            OccupationFragment fragment = new OccupationFragment();
+            fragment = new OccupationFragment();
             //添加fragment到容器中
             fragmentManager.beginTransaction().add(R.id.fl_fragment, fragment, tag).commit();
         }
@@ -80,6 +146,55 @@ public class OccupationActivity extends BaseActivity implements BackPressedHandl
             res = ((BackPressedHandler) fragmentByTag).hasHandlerBackPressed();
         }
         return res;
+    }
+
+    @OnClick({R.id.iv_search, R.id.iv_clear})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_search:
+                Search(mEtSearch.getText().toString());
+                break;
+            case R.id.iv_clear:
+                mEtSearch.setText("");
+                fragment.Search("");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Search(String context) {
+        if (!TextUtils.isEmpty(context)) {
+
+            //去除左右空格
+            context = context.trim();
+
+            //包含特殊字符则不搜索
+            if (DataCheckUtil.containSpecialCharacter(context)) {
+                //通用的搜索文本错误提示
+                commonSearchContentErrorTip(R.string.search_error_tip_special_char);
+                return;
+            }
+
+            //长度只有1的情况
+            if (context.length() < 2) {
+                //通用的搜索文本错误提示
+                commonSearchContentErrorTip(R.string.search_error_tip_length);
+                return;
+            } else {
+                fragment.Search(context);
+                //关闭软键盘
+                SoftInputUtil.closeSoftInput(OccupationActivity.this);
+            }
+        }
+        else {
+            commonSearchContentErrorTip(R.string.search_error_tip_length);
+            return;
+        }
+    }
+
+    private void commonSearchContentErrorTip(int resId) {
+        ToastUtil.customToastGravity(getApplication(), getResources().getString(resId), Toast.LENGTH_SHORT, Gravity.CENTER_VERTICAL, 0, 0);
     }
 
 }
